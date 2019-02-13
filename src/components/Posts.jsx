ba0d436a0ea9f07aaa1ec1573a3843da2b6dab77
walze <catufuzgu@gmail.com>
2019-02-13 17:26:38 -0200

    added slider

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -6,19 +6,29 @@ import { tns } from "tiny-slider/src/tiny-slider"
 
 export class Posts extends React.Component {
 
-  componentDidMount() {
-    if (isMobile())
-      this.slider = tns({
-        container: ".posts",
-        items: 1,
-        slideBy: "page",
-        mouseDrag: true,
-        controls: false,
-        nav: true,
-        navPosition: 'bottom'
-      });
 
+  componentDidMount() {
+    this.slider = tns({
+      container: ".posts",
+      items: 1,
+      slideBy: "page",
+      mouseDrag: true,
+      controls: false,
+      nav: true,
+      navPosition: 'bottom',
+      disable: true
+    });
+
+    if (!isMobile()) this.slider.destroy()
+
+    window.addEventListener('resize', () => {
+      if (isMobile()) return this.slider.rebuild()
+      if (!isMobile()) return this.slider.destroy()
+
+      alert('consertar isso')
+    })
   }
+
 
   render() {
     return (
