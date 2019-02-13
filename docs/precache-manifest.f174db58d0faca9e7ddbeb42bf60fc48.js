4a0030e718fbc0b0d81539209307ba66f6c1f70e
walze <catufuzgu@gmail.com>
2019-02-13 15:07:01 -0200

    added docs

---
+++ docs/precache-manifest.f174db58d0faca9e7ddbeb42bf60fc48.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "./static/media/fontisto.b49ae8ab.ttf"
+  },
+  {
+    "revision": "80bc092bef57222b52e7e3e711b4cdf2",
+    "url": "./static/media/fontisto.80bc092b.eot"
+  },
+  {
+    "revision": "625f3922316e2979d492be778b92743a",
+    "url": "./static/media/fontisto.625f3922.svg"
+  },
+  {
+    "revision": "5b2dc6fc8ca439534ca2b506569fafc3",
+    "url": "./static/media/fontisto.5b2dc6fc.woff"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "364b85aa5c283ee248d4",
+    "url": "./static/js/main.364b85aa.chunk.js"
+  },
+  {
+    "revision": "200ef66e5fb8c0b4976b",
+    "url": "./static/js/2.200ef66e.chunk.js"
+  },
+  {
+    "revision": "364b85aa5c283ee248d4",
+    "url": "./static/css/main.ad2b4713.chunk.css"
+  },
+  {
+    "revision": "200ef66e5fb8c0b4976b",
+    "url": "./static/css/2.9ffe2ad1.chunk.css"
+  },
+  {
+    "revision": "ba93b23248891f758097477356d4d9fb",
+    "url": "./index.html"
+  }
+];
