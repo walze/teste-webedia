d278135ade4e15b100bae15914540966d0a102f1
walze <catufuzgu@gmail.com>
2019-02-15 20:08:47 -0200

    script update, package update

--- server/index.js
+++ server/index.js
@@ -57,6 +57,6 @@ app.post('/posts', async (rq, rs) => {
   })
 })
 
-app.listen(3000, function () {
-  console.log('\nListening on port 3000...')
+app.listen(3001, function () {
+  console.log('\nListening on port 3001...')
 })
